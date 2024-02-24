1e7069e0b3f5bb2765c95f42fea6b590be3581f8
OksanaSokal <sokaloksana@gmail.com>
2024-02-24 20:16:40 +0300

    refactor: add interface, replace news.js with news.ts

---
+++ news-api/src/types.ts
@@ -0,0 +1,34 @@
+export interface NewsTemp {
+  // status: string,
+  // totalResults: number
+  // articles: [
+  //   {
+  //     source: {
+  //       id: boolean,
+  //       name: string
+  //       },
+  //       author: string,
+  //       title: string,
+  //       description: string,
+  //       url: string,
+  //       urlToImage: string,
+  //       publishedAt: string,
+  //       content: string
+  //   }
+  // ]
+  // articles: [
+    // {
+      source: {
+        id: boolean,
+        name: string
+        },
+        author: string,
+        title: string,
+        description: string,
+        url: string,
+        urlToImage: string,
+        publishedAt: string,
+        content: string
+    // }
+  // ]
+}
