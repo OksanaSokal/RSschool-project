eec942069a439f3bb8b3b4e41a563d64461d2bb0
OksanaSokal <sokaloksana@gmail.com>
2024-02-26 11:13:29 +0300

    refactor: replace loader.js with loader.ts

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -1,19 +1,20 @@
+import { Options } from '../../types/loader-type';
 class Loader {
-    constructor(baseLink, options) {
-        this.baseLink = baseLink;
-        this.options = options;
-    }
+    constructor(
+        public baseLink: string,
+        public options: Options
+    ) {}
 
-    getResp(
-        { endpoint, options = {} },
-        callback = () => {
+    public getResp(
+        { endpoint, options = {} }: { endpoint: string; options?: Options },
+        callback = (): void => {
             console.error('No callback for GET response');
         }
-    ) {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    private errorHandler(res: Response) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,7 +24,7 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    private makeUrl(options: Options, endpoint: string) {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
@@ -34,7 +35,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    public load(method: string, endpoint: string, callback: (args: unknown) => void, options = {}): void {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
