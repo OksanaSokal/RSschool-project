00ec872406f822a42b9dd3f59b62eeae3cf87a15
OksanaSokal <sokaloksana@gmail.com>
2024-03-06 21:29:59 +0300

    chore:configure webpack

---
+++ rss-puzzle/webpack.config.ts
@@ -0,0 +1,59 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack');
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+type Mode = 'production' | 'development';
+
+interface EnvVariables {
+  mode: Mode;
+  port: number;
+}
+
+export default (env: EnvVariables) => {
+
+  const isDev = env.mode === 'development';
+  const config: webpack.Configuration = {
+    mode: env.mode ?? 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+      }),
+      new webpack.ProgressPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    devServer: isDev ? {
+      port: env.port ?? 3000,
+      open: true,
+    } : undefined,
+  }
+  return config;
+};
