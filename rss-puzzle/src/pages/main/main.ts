53984f664d4f6a701932da3702fb3cbfd5ec71c4
OksanaSokal <sokaloksana@gmail.com>
2024-03-14 15:37:47 +0300

    feat: implement save data in Local Storage

--- rss-puzzle/src/pages/main/main.ts
+++ rss-puzzle/src/pages/main/main.ts
@@ -12,7 +12,34 @@ const CssClasses = {
   button: 'button',
 };
 
+const paramsLastName: ElementParam = {
+  tag: 'input',
+  className: [CssClasses.input],
+  text: 'Last Name',
+  callback: null,
+};
+
+const paramsButton: ElementParam = {
+  tag: 'button',
+  className: [CssClasses.button],
+  text: 'Login',
+  callback: null,
+};
+
+const paramsFirstName: ElementParam = {
+  tag: 'input',
+  className: [CssClasses.input],
+  text: 'First Name',
+  callback: null,
+};
+
 export default class MainView extends View {
+  private firstNameInput: HTMLInputElement;
+
+  private lastNameInput: HTMLInputElement;
+
+  private button: ButtonView;
+
   constructor() {
     const params: ElementParam = {
       tag: 'main',
@@ -34,36 +61,53 @@ export default class MainView extends View {
     const createForm: ElementCreator = new ElementCreator(paramsForm);
     this.elementCreator.addElement(createForm);
 
-    const paramsFirstName: ElementParam = {
-      tag: 'input',
-      className: [CssClasses.input],
-      text: 'First Name',
-      callback: null,
-    };
     const firstName: InputView = new InputView(paramsFirstName);
+    this.firstNameInput = firstName.inputElement;
     firstName.addLength('3');
-    firstName.setCallback();
+    firstName.addCallback();
     createForm.addElement(firstName);
-
-    const paramsLastName: ElementParam = {
-      tag: 'input',
-      className: [CssClasses.input],
-      text: 'Last Name',
-      callback: null,
-    };
     const lastName: InputView = new InputView(paramsLastName);
+    this.lastNameInput = lastName.inputElement;
+    lastName.addCallback();
     lastName.addLength('4');
-    lastName.checkValue();
-    lastName.setCallback();
     createForm.addElement(lastName);
 
-    const paramsButton: ElementParam = {
-      tag: 'button',
-      className: [CssClasses.button],
-      text: 'Login',
-      callback: null,
-    };
-    const button: ButtonView = new ButtonView(paramsButton);
-    createForm.addElement(button);
+    this.button = new ButtonView(paramsButton);
+    this.button.buttonElement.addEventListener('click', () => {
+      this.button.addLocalStorage(this.firstNameInput.value, this.lastNameInput.value);
+    });
+    createForm.addElement(this.button);
+
+    firstName.inputElement.addEventListener('input', () => {
+      this.updateButtonState();
+    });
+
+    lastName.inputElement.addEventListener('input', () => {
+      this.updateButtonState();
+    });
+  }
+
+  private updateButtonState(): void {
+    this.button.buttonElement.disabled = !this.checkFields();
+    if (this.checkFields()) {
+      this.button.buttonElement.classList.add('button-open');
+    } else {
+      this.button.buttonElement.classList.remove('button-open');
+    }
+  }
+
+  public checkFields(): boolean {
+    const firstNameValue = this.firstNameInput.value.trim();
+    const lastNameValue = this.lastNameInput.value.trim();
+
+    if (firstNameValue && lastNameValue) {
+      const firstNameRegex = /^[A-Z][-a-zA-Z]+$/;
+      const lastNameRegex = /^[A-Z][-a-zA-Z]+$/;
+
+      const firstNameValid = firstNameRegex.test(firstNameValue) && firstNameValue.length >= 3;
+      const lastNameValid = lastNameRegex.test(lastNameValue) && lastNameValue.length >= 4;
+
+      return firstNameValid && lastNameValid;
+    }
   }
 }
