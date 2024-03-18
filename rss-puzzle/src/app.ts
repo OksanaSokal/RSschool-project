9e2e7660851efd996daee405e263b4319bf70a51
OksanaSokal <sokaloksana@gmail.com>
2024-03-18 14:55:53 +0300

    feat: implement welcome page, logout button

--- rss-puzzle/src/app.ts
+++ rss-puzzle/src/app.ts
@@ -4,7 +4,7 @@ import WrapperView from './pages/wrapper/wrapper';
 
 export default class App {
   constructor() {
-    App.createView();
+    App.checkLocalStorage();
   }
 
   static createView() {
@@ -14,5 +14,43 @@ export default class App {
 
     document.body.append(wrapperElement.getHTMLElement());
     wrapperElement.getHTMLElement().append(headerElement.getHTMLElement(), mainElement.getHTMLElement());
+  }
+
+  static checkLocalStorage() {
+    const data = localStorage.getItem('user');
+    if (data) {
+      App.showWelcomePage();
+    } else {
+      App.createView();
+    }
+  }
+
+  static showWelcomePage() {
+    const wrapperElement = new WrapperView();
+    const headerElement = new HeaderView();
+
+    const btnLogout = document.createElement('button');
+    btnLogout.textContent = 'Logout';
+    btnLogout.classList.add('logout');
+    btnLogout.addEventListener('click', () => {
+      localStorage.clear();
+      document.body.innerHTML = '';
+      App.createView();
+    });
+    headerElement.getHTMLElement().append(btnLogout);
+
+    const createMain = document.createElement('main');
+    createMain.classList.add('welcome');
+    const welcomeBlock = document.createElement('div');
+    const data = JSON.parse(localStorage.getItem('user'));
+    welcomeBlock.textContent = `Welcome, ${data.name} ${data.surname}`;
+
+    const button = document.createElement('button');
+    button.classList.add('button', 'button-open');
+    button.textContent = 'START';
+    createMain.append(welcomeBlock, button);
+    createMain.classList.add('main');
+    document.body.append(wrapperElement.getHTMLElement());
+    wrapperElement.getHTMLElement().append(headerElement.getHTMLElement(), createMain);
   }
 }
