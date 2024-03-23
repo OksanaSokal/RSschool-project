4624dbc5896bf02857f5d7b761a3114e55ba1c42
OksanaSokal <sokaloksana@gmail.com>
2024-03-23 12:20:49 +0300

    init: start async-race task, add configuration

---
+++ async-race/src/index.ts
@@ -0,0 +1,27 @@
+// import  image from './components/img/car.svg';
+
+// const img = document.createElement('img');
+// img.src = image;
+// document.body.append(img)
+
+import './style.css';
+
+// const body = document.createElement('div');
+document.body.classList.add('body');
+
+// async function getCarById(carId) {
+//   try {
+//     const url = `http://127.0.0.1:3000/garage/${carId}`;
+
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Error: ${response.status}`);
+//     }
+//     const carData = await response.json();
+//     console.log('Car data:', carData);
+//   } catch (error) {
+//     console.error('Failed to fetch car data:', error);
+//   }
+// }
+
+// getCarById(1);
